365205c5babcf3c679ef2a8ecf99060ae1b5a3d4
mohamed12samy <mohamedsamyabbas99@gmail.com>
2023-10-12 12:28:35 +0300

    continue preparing solution

--- HomeEducation/src/Infrastructure/Persistence/ApplicationDbContext.cs
+++ HomeEducation/src/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,7 +1,10 @@
 ﻿using System.Reflection;
+using System.Reflection.Emit;
+using System.Reflection.Metadata;
 using Duende.IdentityServer.EntityFramework.Options;
 using HomeEducation.Application.Common.Interfaces;
 using HomeEducation.Domain.Entities;
+using HomeEducation.Domain.Enums;
 using HomeEducation.Infrastructure.Identity;
 using HomeEducation.Infrastructure.Persistence.Interceptors;
 using MediatR;
@@ -26,14 +29,28 @@ public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>, 
         _auditableEntitySaveChangesInterceptor = auditableEntitySaveChangesInterceptor;
     }
 
-    public DbSet<TodoList> TodoLists => Set<TodoList>();
-
-    public DbSet<TodoItem> TodoItems => Set<TodoItem>();
+    public DbSet<Level> Levels => Set<Level>();
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        builder.Entity<Level>().HasData(
+            new Level { Id="1Primary",Title = "First", Phase = StudyPhase.Primary  },
+            new Level { Id="2Primary",Title = "Second", Phase = StudyPhase.Primary  },
+            new Level { Id="3Primary",Title = "Third", Phase = StudyPhase.Primary  },
+            new Level { Id="4Primary",Title = "Fourth", Phase = StudyPhase.Primary  },
+            new Level { Id="5Primary",Title = "Fifth", Phase = StudyPhase.Primary  },
+            new Level { Id="6Primary",Title = "Sixth", Phase = StudyPhase.Primary  },
 
+            new Level { Id = "1Prepare", Title = "First", Phase = StudyPhase.Preparatory },
+            new Level { Id = "2Prepare", Title = "Second", Phase = StudyPhase.Preparatory },
+            new Level { Id = "3Prepare", Title = "Third", Phase = StudyPhase.Preparatory },
+
+            new Level { Id = "1Secondary", Title = "First", Phase = StudyPhase.Secondary },
+            new Level { Id = "2Secondary", Title = "Second", Phase = StudyPhase.Secondary },
+            new Level { Id = "3Secondary", Title = "Third", Phase = StudyPhase.Secondary }
+
+            );
         base.OnModelCreating(builder);
     }
 
--- HomeEducation/src/WebApi/ConfigureServices.cs
+++ HomeEducation/src/WebApi/ConfigureServices.cs
@@ -20,7 +20,7 @@ public static class ConfigureServices
         services.AddHealthChecks()
             .AddDbContextCheck<ApplicationDbContext>();
 
-        services.AddControllersWithViews();
+        //services.AddControllersWithViews();
 
        // services.AddRazorPages();
 
